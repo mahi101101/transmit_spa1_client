276d02c30abd20f4f5bb0520db2a2e9f553b07d4
M Mahi Pavan Teja Sathguna Sai <mpavanteja2001@gmail.com>
2024-04-04 14:38:01 +0530

    Login page added

--- src/Components/User/SignUp.js
+++ src/Components/User/SignUp.js
@@ -267,7 +267,6 @@ const SignUp = () => {
   return (
     <React.Fragment>
       <MetaData title={"Registration Form"} />
-      {/* <ToastContainer /> */}
       <Row className="f-box justify-content-center m-0 mt-4 ">
         <Col md="6" className="border border-secondary-subtle rounded mb-5">
           <Col md="12" className="text-center p-5">
